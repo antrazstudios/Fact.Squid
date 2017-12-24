5129640b3c43ef096cbed9c3d72cc34abda4dd0a
CarlosVasquez <ingcarlosvasquez@outlook.com>
2017-12-23 22:22:37 -0500

    Mejora del procesamiento de solicitudes
    Se mejora la manera en que se obtienen los datos desde la base de datos de origen

--- src/renderer/libs/storage.js
+++ src/renderer/libs/storage.js
@@ -1,6 +1,10 @@
 const q = require('q')
 
+// -----------------------------------------------------------------------------------------------------------
 exports.testConnection = (hostVar, portVar, userVar, passVar, databaseVar, _callback) => {
+  // --------------------------| Description |--------------------------
+  // Description: Realiza una prueba de una conexion
+  // ------------------------| End Description |------------------------
   let mysql = require('mysql')
   let mysqlConnection = mysql.createConnection({
     port: portVar,
@@ -20,10 +24,11 @@ exports.testConnection = (hostVar, portVar, userVar, passVar, databaseVar, _call
   })
 }
 
-/**
-  * Crea una nueva conexion, con defaultConn
-*/
+// -----------------------------------------------------------------------------------------------------------
 exports.getActualConnection = () => {
+  // --------------------------| Description |--------------------------
+  // Description: Obtiene la conexion actual del sistema
+  // ------------------------| End Description |------------------------
   let mysql = require('mysql')
   let settings = require('./settings.js')
   let connObj = settings.getConnectionbyId(settings.getContentFromLocalKey('defaultConn'))
@@ -36,7 +41,49 @@ exports.getActualConnection = () => {
   })
 }
 
+// -----------------------------------------------------------------------------------------------------------
+exports._database_runQuery = (configuracion) => {
+  // --------------------------| Description |--------------------------
+  // Description: Corre un query que puede o no regresar una serie de datos
+  // Parameters:
+  // * configuracion. query = Es el query que se correra en el servidor
+  // * configuracion. useActualConn = Define si se utiliza la conexion actual del sistema o si utiliza otra, por defecto esta propiedad esta establecida en true
+  // * configuracion. alterConn = Define una conexion alternativa en caso de definir la propiedad useActualConn en false
+  // ------------------------| End Description |------------------------
+  let deferred = q.defer()
+  let conn = configuracion.useActualConn === false ? configuracion.alterConn : this.getActualConnection()
+  // se intenta realizar la conexion
+  conn.connect((err) => {
+    if (err) { // en caso de presentarse un error se notifica a la promesa de ello
+      deferred.reject('Hha ocurrido un error al intentar conectarse al servidor: ' + conn.host + ' => ' + err)
+    }
+  })
+  // Se ejecuta el respectivo query con la consulta, y retorna el valor de la misma
+  conn.query(configuracion.query, (err, results, fields) => {
+    if (err !== null && err !== '') {
+      deferred.reject('Ha ocurrido un error al ejecutar la consulta o el procedimiento en el servidor: ' + conn.host + ' => ' + err)
+    } else {
+      deferred.resolve({
+        message: 'Operacion ejecuta con exito en el servidor',
+        result: results,
+        field: fields
+      })
+    }
+  })
+  // Finalizamos la conexion actual con el servidor
+  conn.end()
+  // Regresa como respuesta una promesa
+  return deferred.promise
+}
+
+// -----------------------------------------------------------------------------------------------------------
 exports._database_usersLoginWithNicknameAndPass = (configuracion) => {
+  // --------------------------| Description |--------------------------
+  // Description: Proceso desde la base de datos obtiene los datos del usuario
+  // Parameters:
+  // * configuracion. username = nombre de usuarioa iniciar sesion
+  // * configuracion. pass= contraseña del usuario a validar
+  // ------------------------| End Description |------------------------
   // Definicion de los datos a retornar en caso de ser positiva la respuesta
   // del servidor
   let data = {
@@ -44,25 +91,24 @@ exports._database_usersLoginWithNicknameAndPass = (configuracion) => {
     permissionsConsult: []
   }
   // Bandera de verificacion de mensaje
-  let isSuccess = true
-  // Definicion de variables de Q para generar promesas dentro de un modulo
+  // let isSuccess = true
+  // // Definicion de variables de Q para generar promesas dentro de un modulo
   let deferred = q.defer()
-  // Definicion de la conexion actual del sistema
-  let actualConn = this.getActualConnection()
+  // // Definicion de la conexion actual del sistema
+  // let actualConn = this.getActualConnection()
   // Se intenta realizar la conexion al sistema
-  actualConn.connect((err) => {
-    if (err) {
-      deferred.reject('Ha ocurrido un error al intentar realizar la conexion: ' + err)
-    }
-  })
-  // Se ejecuta el respectivo query con la consulta, y retorna el valor de la misma
-  actualConn.query('call usersLoginWithNicknameAndPass(\'' + configuracion.username + '\', \'' + configuracion.pass + '\');', (err, results, fields) => {
-    let resultData1 = results[0]
-    let resultData2 = results[1]
-    let resultData3 = results[2]
+  // actualConn.connect((err) => {
+  //   if (err) {
+  //     deferred.reject('Ha ocurrido un error al intentar realizar la conexion: ' + err)
+  //   }
+  // })
+  this._database_runQuery({ query: 'call usersLoginWithNicknameAndPass(\'' + configuracion.username + '\', \'' + configuracion.pass + '\');' }).then((rta) => {
+    let resultData1 = rta.result[0]
+    let resultData2 = rta.result[1]
+    let resultData3 = rta.result[2]
     // Verificacion de la cantidad de resultados obtenidos desde la base de datos,
     // en caso de obtener 3 tablas de datos y la del sistema crear el token y permitir el inicio de sesion
-    if (results.length === 4) {
+    if (rta.result.length === 4) {
       // Se recorren todos los permisos del usuario
       for (let i = 0; i < resultData3.length; i++) {
         data.permissionsConsult.push(require('./objects.js').createPermissionsToken(resultData3[i].tb_permission_content, resultData3[i].tb_permission_description))
@@ -70,90 +116,99 @@ exports._database_usersLoginWithNicknameAndPass = (configuracion) => {
       // Se obtienen los datos del usuario, y se agregan los permisos obtenidos
       data.userConsult = require('./objects.js').createUserToken(resultData2[0].idtb_users, resultData2[0].tb_users_identificacion, resultData2[0].tb_tiposidentificacion_nombre, resultData2[0].tb_users_primernombre, resultData2[0].tb_users_segundonombre, resultData2[0].tb_users_primerapellido, resultData2[0].tb_users_segundoapellido, resultData2[0].tb_users_username, resultData2[0].tb_cargos_nombre, resultData2[0].tb_users_fechanacimiento, resultData2[0].tb_users_imagenperfil, resultData2[0].tb_oficinas_nombre, resultData2[0].tb_users_isactive, data.permissionsConsult)
       deferred.resolve(data)
-      isSuccess = false
-    } else if (results.length === 3) {
+    } else if (rta.result.length === 3) {
       deferred.reject('Su usuario aun no esta preparado para ser utilizado, pongase en contacto con el administrador del sistema.')
+    } else {
+      deferred.reject('Respuesta del servidor => ' + resultData1[0].typeMessage + ': ' + resultData1[0].messageMessage)
     }
-    // Verificacion de los mensajes del sistema
-    if (isSuccess === true) {
-      if (err) {
-        deferred.reject('Ha ocurrido un error al intentar consultar la informacion en el servidor: ' + err)
-      } else {
-        deferred.reject('Respuesta del servidor => ' + resultData1[0].typeMessage + ': ' + resultData1[0].messageMessage)
-      }
-    }
+  }).catch((err) => {
+    deferred.reject(err)
   })
-  // Finalizamos la conexion
-  actualConn.end()
+  // // Se ejecuta el respectivo query con la consulta, y retorna el valor de la misma
+  // actualConn.query('call usersLoginWithNicknameAndPass(\'' + configuracion.username + '\', \'' + configuracion.pass + '\');', (err, results, fields) => {
+  //   let resultData1 = results[0]
+  //   let resultData2 = results[1]
+  //   let resultData3 = results[2]
+  //   // Verificacion de la cantidad de resultados obtenidos desde la base de datos,
+  //   // en caso de obtener 3 tablas de datos y la del sistema crear el token y permitir el inicio de sesion
+  //   if (results.length === 4) {
+  //     // Se recorren todos los permisos del usuario
+  //     for (let i = 0; i < resultData3.length; i++) {
+  //       data.permissionsConsult.push(require('./objects.js').createPermissionsToken(resultData3[i].tb_permission_content, resultData3[i].tb_permission_description))
+  //     }
+  //     // Se obtienen los datos del usuario, y se agregan los permisos obtenidos
+  //     data.userConsult = require('./objects.js').createUserToken(resultData2[0].idtb_users, resultData2[0].tb_users_identificacion, resultData2[0].tb_tiposidentificacion_nombre, resultData2[0].tb_users_primernombre, resultData2[0].tb_users_segundonombre, resultData2[0].tb_users_primerapellido, resultData2[0].tb_users_segundoapellido, resultData2[0].tb_users_username, resultData2[0].tb_cargos_nombre, resultData2[0].tb_users_fechanacimiento, resultData2[0].tb_users_imagenperfil, resultData2[0].tb_oficinas_nombre, resultData2[0].tb_users_isactive, data.permissionsConsult)
+  //     deferred.resolve(data)
+  //     isSuccess = false
+  //   } else if (results.length === 3) {
+  //     deferred.reject('Su usuario aun no esta preparado para ser utilizado, pongase en contacto con el administrador del sistema.')
+  //   }
+  //   // Verificacion de los mensajes del sistema
+  //   if (isSuccess === true) {
+  //     if (err) {
+  //       deferred.reject('Ha ocurrido un error al intentar consultar la informacion en el servidor: ' + err)
+  //     } else {
+  //       deferred.reject('Respuesta del servidor => ' + resultData1[0].typeMessage + ': ' + resultData1[0].messageMessage)
+  //     }
+  //   }
+  // })
+  // // Finalizamos la conexion
+  // actualConn.end()
   // Regresa como respuesta una promesa
   return deferred.promise
 }
 
-exports._consultTerceros = (configuracion) => {
-  // Configuracion recibe un item type que puede ser 'juridica' o 'natural'
+// -----------------------------------------------------------------------------------------------------------
+exports._database_consultTerceros = (configuracion) => {
+  // --------------------------| Description |--------------------------
+  // Description: Proceso desde la base de datos que obtiene un listado de usuarios
+  // Parameters:
+  // * configuracion. type = 'juridica' || 'natural'
+  // ------------------------| End Description |------------------------
   // Definicion de datos de resultado desde el servidor
   let terceros = []
-  // Bandera de verificacion de mensajes
-  let isSuccess = true
   // Definicion de variables de Q para generar promesas dentro de un modulo
   let deferred = q.defer()
-  // Definicion de la conexion actual del sistema
-  let actualConn = this.getActualConnection()
-  // Se intenta realizar la conexion al sistema
-  actualConn.connect((err) => {
-    if (err) {
-      deferred.reject('Ha ocurrdio un error al intentar realizar la conexion: ' + err)
-    }
-  })
-  // Se ejecuta el respectivo query con la consulta y retorna el valor de la misma
-  actualConn.query(configuracion.type === 'juridica' ? 'SELECT * FROM consultTercerosJuridicas;' : 'SELECT * FROM consultTercerosNaturales;', (err, results, fields) => {
-    // Verificacion de la cantidad de resultados obtenidos desde la base de datos
-    if (results.length !== 0 && results.length !== null) {
-      for (let i = 0; i < results.length; i++) {
+  // ejecutamos el proceso de obtencion de datos en el servidor
+  this._database_runQuery({
+    query: configuracion.type === 'juridica' ? 'SELECT * FROM consultTercerosJuridicas;' : 'SELECT * FROM consultTercerosNaturales;'
+  }).then((rta) => {
+    // Verficacion de la cantidad de resultados obtenidos desde la base de datos
+    if (rta.result !== 0 && rta.result !== null) {
+      for (let i = 0; i < rta.result.length; i++) {
         if (configuracion.type === 'juridica') {
-          terceros.push(require('./objects.js').createTercerosJuridica(results[i].idtb_tercerosjuridicas, results[i].tb_tercerosjuridicas_razonsocial, results[i].tb_tercerosjuridicas_representantelegal, require('./objects.js').createTerceros(results[i].idtb_terceros, require('./objects.js').createTiposIdentificacion(results[i].tb_terceros_tipodocumento, results[i].tb_tiposidentificacion_nombre, null), results[i].tb_terceros_numerodocumento, results[i].tb_terceros_isactive)))
+          terceros.push(require('./objects.js').createTercerosJuridica(rta.result[i].idtb_tercerosjuridicas, rta.result[i].tb_tercerosjuridicas_razonsocial, rta.result[i].tb_tercerosjuridicas_representantelegal, require('./objects.js').createTerceros(rta.result[i].idtb_terceros, require('./objects.js').createTiposIdentificacion(rta.result[i].tb_terceros_tipodocumento, rta.result[i].tb_tiposidentificacion_nombre, null), rta.result[i].tb_terceros_numerodocumento, rta.result[i].tb_terceros_isactive)))
         } else {
-          terceros.push(require('./objects.js').createTercerosNatural(results[i].idtb_tercerosjuridicas, results[i].tb_tercerospersonas_primernombre, results[i].tb_tercerospersonas_segundonombre, results[i].tb_tercerospersonas_primerapellido, results[i].tb_tercerospersonas_segundoapellido, require('./objects.js').createTerceros(results[i].idtb_terceros, require('./objects.js').createTiposIdentificacion(results[i].tb_terceros_tipodocumento, results[i].tb_tiposidentificacion_nombre, null), results[i].tb_terceros_numerodocumento, results[i].tb_terceros_isactive)))
+          terceros.push(require('./objects.js').createTercerosNatural(rta.result[i].idtb_tercerosjuridicas, rta.result[i].tb_tercerospersonas_primernombre, rta.result[i].tb_tercerospersonas_segundonombre, rta.result[i].tb_tercerospersonas_primerapellido, rta.result[i].tb_tercerospersonas_segundoapellido, require('./objects.js').createTerceros(rta.result[i].idtb_terceros, require('./objects.js').createTiposIdentificacion(rta.result[i].tb_terceros_tipodocumento, rta.result[i].tb_tiposidentificacion_nombre, null), rta.result[i].tb_terceros_numerodocumento, rta.result[i].tb_terceros_isactive)))
         }
+        deferred.resolve(terceros)
       }
-      deferred.resolve(terceros)
-      isSuccess = false
     } else {
       deferred.reject('La consulta no ha arrojado resultados')
     }
-    // Verificacion de la no ejecucion
-    if (isSuccess === true) {
-      if (err) {
-        deferred.reject('No ha sido posible ejecutar la consulta: ' + err)
-      }
-    }
+  }).catch((err) => {
+    deferred.reject(err)
   })
-  // Finalizamos la conexion
-  actualConn.end()
-  // Regresamos como respuesta una promesa
+  // Retornamos una promesa como respuesta
   return deferred.promise
 }
 
-exports._changeStateTerceros = (configuracion) => {
+// -----------------------------------------------------------------------------------------------------------
+exports._database_changeStateTerceros = (configuracion) => {
+  // --------------------------| Description |--------------------------
+  // Description: Proceso desde la base de dats que cambia el estado de un tercero
+  // Parameters:
+  // * configuracion. id = llave de la tabla de terceros del tercero a cambiar
+  // * configuracion. state = estado a aplicarse al tercero
+  // ------------------------| End Description |------------------------
   // Definicion de variables de Q para generar promesas dentro de un modulo
   let deferred = q.defer()
-  // Definicion de la conexion actual del sistema
-  let actualConn = this.getActualConnection()
-  // Se ejecuta el respetivo query con la accion
-  actualConn.connect((err) => {
-    if (err) {
-      deferred.reject('Ha ocurrido un error al intentar realizar la conexion: ' + err)
-    }
+  // Se ejecuta el respectivo query con la accion
+  this._database_runQuery({ query: 'call changeStateTercero (\'' + configuracion.id + '\', \'' + configuracion.state + '\');' }).then(() => {
+    deferred.resolve('Tercero actualizado')
+  }).catch((err) => {
+    deferred.reject(err)
   })
-  actualConn.query('call changeStateTercero (\'' + configuracion.id + '\', \'' + configuracion.state + '\');', (err, results, fields) => {
-    if (err !== null) {
-      console.log('test')
-      deferred.reject('Ha ocurrido un error al intentar cambiar el estado del tercero: ' + err)
-    }
-    if (err === null) {
-      deferred.resolve('Tercero actualizado correctamente')
-    }
-  })
-  actualConn.end()
+  // Se retorna la promesa
   return deferred.promise
 }
